a5e3bdba518f6a4e0724acba77258b33e1ccea25
haaeell <haikaljawahir434@gmail.com>
2026-05-18 01:40:06 +0700

    ads

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,12 +15,12 @@ class DatabaseSeeder extends Seeder
         $tanggalAwal = '2025-01-02';
 
         DB::table('pengaturan_toko')->insert([
-            'nama_toko'            => 'Toko Obat Tani Makmur Jaya',
-            'alamat'               => 'Jl. Raya Pertanian No. 45, Sleman, Yogyakarta',
+            'nama_toko'            => 'Toko XYZ',
+            'alamat'               => 'Jl. Test No. 45, Sleman, Yogyakarta',
             'telepon'              => '0274-123456',
             'email'                => 'tokoobattani.makmurajaya@gmail.com',
             'logo'                 => null,
-            'header_struk'         => "TOKO OBAT TANI MAKMUR JAYA\nJl. Raya Pertanian No. 45, Sleman\nTelp: 0274-123456",
+            'header_struk'         => "TOKO XYZ\nJl. Test No. 45, Sleman\nTelp: 0274-123456",
             'footer_struk'         => "Terima kasih telah berbelanja!\nBarang yang sudah dibeli tidak dapat dikembalikan.",
             'stok_minimum_default' => 10,
             'created_at'           => $now,
